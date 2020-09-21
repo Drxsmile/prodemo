472c4f92fdc2599e7281c5ef1aa3b5a371cec6c1
snx <snx1029@163.com>
2020-09-21 15:24:42 +0800

    protect secret key

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's+3zan3$q+(%hga($&k-&!xfr2ukgu+ph#93p+&3t=mndqv$&&'
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
